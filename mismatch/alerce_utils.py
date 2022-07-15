d9fca7a8ddfe970e32991468f1cde3abdac642f8
opimentel-tesla <oscarlo.pimentel@gmail.com>
2022-07-15 15:26:31 -0400

    set()

--- mismatch/alerce_utils.py
+++ mismatch/alerce_utils.py
@@ -11,45 +11,45 @@ N_DASK = _C.N_DASK
 ###################################################################################################################################################
 
 def isin_filter_df(df, index, objs,
-	inverse=False,
-	):
-	df = df.reset_index()
-	df = df[~df[index].isin(objs) if inverse else df[index].isin(objs)]
-	df = df.set_index(index)
-	return df
+    inverse=False,
+    ):
+    df = df.reset_index()
+    df = df[~df[index].isin(objs) if inverse else df[index].isin(objs)]
+    df = df.set_index(index)
+    return df
 
 def get_train_test_split(df, labels_df, index):
-	train_objs = list(set(labels_df.index.values))
-	train_df = isin_filter_df(df, index, train_objs)
-	test_df = isin_filter_df(df, index, train_objs, inverse=True)
-	return train_df, test_df
+    train_objs = sorted(list(set(labels_df.index.values)))
+    train_df = isin_filter_df(df, index, train_objs)
+    test_df = isin_filter_df(df, index, train_objs, inverse=True)
+    return train_df, test_df
 
 def filter_by_valid_objs(df, valid_objs):
-	return df[df.index.isin(valid_objs)]
+    return df[df.index.isin(valid_objs)]
 
 def keep_only_valid_objs(df, valid_objs):
-	return df[df.index.isin(valid_objs)]
-	
+    return df[df.index.isin(valid_objs)]
+    
 def drop_duplicates(df,
-	npartitions=N_DASK,
-	):
-	ddf = dd.from_pandas(df, npartitions=npartitions)
-	return ddf.drop_duplicates().compute() # FAST
-	
+    npartitions=N_DASK,
+    ):
+    ddf = dd.from_pandas(df, npartitions=npartitions)
+    return ddf.drop_duplicates().compute() # FAST
+    
 ###################################################################################################################################################
 
 def process_df_detections(df, index_name, new_index_name):
-	assert df.index.name==index_name
-	df.index.rename(new_index_name, inplace=True) # rename index
-	df = df.reset_index()
-	df = drop_duplicates(df)
-	df = df.set_index([new_index_name])
-	objs = list(set(df.index))
-	return df, objs
+    assert df.index.name==index_name
+    df.index.rename(new_index_name, inplace=True) # rename index
+    df = df.reset_index()
+    df = drop_duplicates(df)
+    df = df.set_index([new_index_name])
+    objs = sorted(list(set(df.index)))
+    return df, objs
 
 def process_df_labels(df, new_index_name, det_objs):
-	df = drop_duplicates(df)
-	df = df.set_index([new_index_name])
-	df = filter_by_valid_objs(df, det_objs) # clean labels dataframe
-	objs = list(set(df.index))
-	return df, objs
+    df = drop_duplicates(df)
+    df = df.set_index([new_index_name])
+    df = filter_by_valid_objs(df, det_objs) # clean labels dataframe
+    objs = sorted(list(set(df.index)))
+    return df, objs
--- mismatch/utils.py
+++ mismatch/utils.py
@@ -8,22 +8,22 @@ import pandas as pd
 ###################################################################################################################################################
 
 def get_object_features(features_ddf, labels_ddf, obj_name,
-	band=None,
-	bands=['1','2'],
-	):
-	df = features_ddf.loc[obj_name].compute() # FAST
-	if band is None: # get all
-		pass
-	elif band>=0: # get band-wise features
-		df = df[[c for c in df.columns if c[-2:]==f'_{band}']]
-	else: # get non-band-wise features
-		bands_ = [f'_{b}' for b in bands]
-		df = df[[c for c in df.columns if not c[-2:] in bands_]]
+    band=None,
+    bands=['1','2'],
+    ):
+    df = features_ddf.loc[obj_name].compute() # FAST
+    if band is None: # get all
+        pass
+    elif band>=0: # get band-wise features
+        df = df[[c for c in df.columns if c[-2:]==f'_{band}']]
+    else: # get non-band-wise features
+        bands_ = [f'_{b}' for b in bands]
+        df = df[[c for c in df.columns if not c[-2:] in bands_]]
 
-	features = df.values[0]
-	features_names = df.columns
-	try:
-		c = labels_ddf.loc[obj_name].compute()['classALeRCE'].values[0] # FAST
-	except IndexError:
-		c = None
-	return features, c, features_names
+    features = df.values[0]
+    features_names = df.columns
+    try:
+        c = labels_ddf.loc[obj_name].compute()['classALeRCE'].values[0] # FAST
+    except IndexError:
+        c = None
+    return features, c, features_names
